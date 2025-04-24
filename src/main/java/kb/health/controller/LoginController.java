452324b2df9f8e6970307b796b408e4a899364e4
Kwon SJ <soonjoo1996@gmail.com>
2025-04-25 08:44:01 +0900

    컨트롤러 브렌치 생성

--- src/main/java/kb/health/controller/LoginController.java
+++ src/main/java/kb/health/controller/LoginController.java
@@ -1,25 +1,20 @@
 package kb.health.controller;
 import kb.health.Service.MemberService;
-import kb.health.authentication.JwtUtil;
-import kb.health.controller.request.LoginRequest;
 import kb.health.domain.Member;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class LoginController {
 
     private final MemberService memberService;
-    private final JwtUtil jwtUtil;
 
     @GetMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
-        String account = loginRequest.getAccount();
-        String password = loginRequest.getPassword();
-
+    public String login() {
+        return "login";
     }
 
     @PostMapping("/login")
@@ -41,7 +36,6 @@ public class LoginController {
     @ExceptionHandler(MatchException.class)
     public String handleMatchException(MatchException e, Model model) {
         model.addAttribute("error", e.getMessage());
-        switch ()
         return "redirect:/login";
     }
 
--- src/main/java/kb/health/controller/ProfileController.java
+++ src/main/java/kb/health/controller/ProfileController.java
@@ -1,39 +1,12 @@
 package kb.health.controller;
 
 
-import kb.health.Service.MemberService;
-import kb.health.authentication.JwtUtil;
-import kb.health.domain.Member;
-import kb.health.domain.MemberForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/profile")
 @RequiredArgsConstructor
 public class ProfileController {
-
-    private final MemberService memberService;
-    private final JwtUtil jwtUtil;
-
-    @GetMapping("/{member_id}")
-    public String profile(@PathVariable("member_id") String memberId, Model model) {
-
-    }
-
-    @GetMapping("/{member_id}/edit")
-    public String edit(@PathVariable("member_id") String memberId, Model model) {
-
-    }
-
-    @PostMapping("/{member_id}/edit")
-    public String editRequset(@PathVariable("member_id") long memberId, @ModelAttribute MemberForm memberForm, Model model) {
-        memberService.updateMember(memberId , memberForm);
-        return "redirect:/profile/" + memberId;
-    }
-
 
 
 
