42d117e1b5ba2ab27d3ce1cc1b8be92ece5c69e5
SwnBae <128581113+SwnBae@users.noreply.github.com>
2025-04-25 14:32:39 +0900

    fix: Diet 컬럼 추가

--- src/main/java/kb/health/domain/record/Diet.java
+++ src/main/java/kb/health/domain/record/Diet.java
@@ -20,4 +20,15 @@ public class Diet {
 
     @Column(nullable = false)
     private int calories;
+
+    private String category;
+
+    private double standardAmount;  // 예: 100g
+
+    private double protein;         // 예: 6.7
+    private double fat;             // 예: 5.16
+    private double carbohydrates;   // 예: 15.94
+    private double sugars;          // 예: 0.16
+    private double fiber;           // 예: 0.7
+    private double sodium;          // 예: 181
 }
