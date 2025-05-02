b71686c0d2c25e97c0141410b5132341a41c70bb
SwnBae <128581113+SwnBae@users.noreply.github.com>
2025-05-02 15:02:44 +0900

    fix: LoginController -> MemberController 이름 변경

--- src/main/java/kb/health/controller/MemberController.java
+++ src/main/java/kb/health/controller/MemberController.java
@@ -10,14 +10,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
-public class LoginController {
+public class MemberController {
 
     private final MemberService memberService;
     private final JwtUtil jwtUtil;
