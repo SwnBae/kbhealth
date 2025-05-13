e3c9d32370d6c98d5dcc671dd860460c7d9db3e0
Kwon SJ <soonjoo1996@gmail.com>
2025-05-13 09:24:00 +0900

    jwt key 생성, feed기능 수정 250513

--- src/main/java/kb/health/service/FeedService.java
+++ src/main/java/kb/health/service/FeedService.java
@@ -21,8 +21,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.io.Writer;
 import java.util.*;
 
 @Service
