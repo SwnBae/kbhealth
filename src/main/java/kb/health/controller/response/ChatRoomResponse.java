3abd472b2b1ead0af82f43416d82eceb88fd8070
SwnBae <128581113+SwnBae@users.noreply.github.com>
2025-05-26 17:34:32 +0900

    fix: 채팅방 상대 정보 누르면 프로필로 이동

--- src/main/java/kb/health/controller/response/ChatRoomResponse.java
+++ src/main/java/kb/health/controller/response/ChatRoomResponse.java
@@ -10,6 +10,8 @@ public class ChatRoomResponse {
     private final Long partnerId;
     private final String partnerName;
     private final String partnerProfileImage;
+    private String partnerAccount;
+    private Double partnerBaseScore;
     private final String lastMessage;
     private final LocalDateTime lastMessageTime;
     private final Long lastMessageSenderId;
@@ -17,12 +19,15 @@ public class ChatRoomResponse {
     private final long unreadCount;
 
     public ChatRoomResponse(String chatRoomId, Long partnerId, String partnerName, String partnerProfileImage,
+                            String partnerAccount, Double partnerBaseScore,
                             String lastMessage, LocalDateTime lastMessageTime, Long lastMessageSenderId,
                             String lastMessageSenderName, long unreadCount) {
         this.chatRoomId = chatRoomId;
         this.partnerId = partnerId;
         this.partnerName = partnerName;
         this.partnerProfileImage = partnerProfileImage;
+        this.partnerAccount = partnerAccount;
+        this.partnerBaseScore = partnerBaseScore;
         this.lastMessage = lastMessage;
         this.lastMessageTime = lastMessageTime;
         this.lastMessageSenderId = lastMessageSenderId;
@@ -36,6 +41,8 @@ public class ChatRoomResponse {
                 chatRoomInfo.getPartner().getId(),
                 chatRoomInfo.getPartner().getUserName(),
                 chatRoomInfo.getPartner().getProfileImageUrl(),
+                chatRoomInfo.getPartner().getAccount(),
+                chatRoomInfo.getPartner().getBaseScore(),
                 chatRoomInfo.getLastMessage().getContent(),
                 chatRoomInfo.getLastMessage().getCreatedDate(),
                 chatRoomInfo.getLastMessage().getSender().getId(),
